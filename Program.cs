50b64446f94cca7a40126b7c627d005bc7b0c86d
gurinenko_n <gurinenko_n@IT-DEP-3>
2017-01-16 16:15:21 +0300

    add SocketServer (TCP)

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AServer
+{
+    class Program
+    {
+        public static string ip;
+        public static int port;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "AServer";
+            try
+            {
+                int i = 0;
+                while (i != args.Length)
+                {
+                    if (args[i].ToString().Contains("ip:"))
+                    {
+                        ip = args[i].ToString().Substring(3, args[i].ToString().Length - 3);
+                        Console.WriteLine("IP: "+ip.ToString());
+                    }
+                    if (args[i].ToString().Contains("port:"))
+                    {
+                        port = Convert.ToInt32(args[i].ToString().Substring(5, args[i].ToString().Length - 5));
+                        Console.WriteLine("PORT: " + port.ToString());
+                    }
+                    i += 1;
+                }
+                Console.WriteLine("Загрузка параметров завершена!");
+            }
+            catch(Exception _Exception)
+            {
+                Console.WriteLine("Возникло исключение при загрузке параметров: "+_Exception.Message);
+            }
+            SocketServer _SocketServer = new SocketServer();
+
+            Console.Read();
+        }
+    }
+}
---
+++ SocketServer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace AServer
+{
+    class SocketServer
+    {
+        static TcpListener listener;
+
+        public SocketServer()
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse(Program.ip), Program.port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(client);
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception _Exception)
+            {
+                Console.WriteLine("Во время подключения возникла ошибка: " + _Exception);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+    }
+
+    public class ClientObject
+    {
+        public TcpClient client;
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+
+        public void Process()
+        {
+            NetworkStream stream = null;
+            try
+            {
+                stream = client.GetStream();
+                byte[] data = new byte[64]; // буфер для получаемых данных
+                while (true)
+                {
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+
+                    message = "NoData";
+                    data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    stream.Close();
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+    }
+}
