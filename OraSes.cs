b7a305bd323549958626897117d6f56115a5c61b
gurinenko_n <gurinenko_n@IT-DEP-3>
2017-01-16 18:44:40 +0300

    16.01.2017

---
+++ OraSes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OracleClient;
+
+namespace AServer
+{
+    public class OraSes
+    {
+        OracleConnection con;
+
+        void Connect()
+        {
+            con = new OracleConnection();
+            con.ConnectionString = "User Id=<username>;Password=<password>;Data Source=<datasource>";
+            con.Open();
+            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
+        }
+    }
+}
